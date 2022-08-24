10020b9e268841d22e56ac2d3092cce62c5e6207
David Augusto <davidribeiro086@gmail.com>
2022-08-24 11:07:18 -0300

    style: Info Section

---
+++ src/pages/LandingPage/components/BrandSection/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const BrandsSectionContainer = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  align-items: center;
+  align-items: center;
+  justify-content: space-evenly;
+  justify-items: center;
+  img {
+    max-width: 10rem;
+  }
+`
