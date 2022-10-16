10f6df88c743bfa52dce9df461bbf1d08e059545
David Augusto <davidribeiro086@gmail.com>
2022-10-16 15:58:16 -0300

    fix: api link

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: 'https://codifyy.azurewebsites.net/api',
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://codifyy.azurewebsites.net/api',
+  // baseURL: 'http://localhost:3000/api',
 })
